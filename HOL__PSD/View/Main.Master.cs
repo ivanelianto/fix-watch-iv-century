aa4f3917d6f11583b827caca224d8e091708155e
Ivan Elv <purenaturalism@gmail.com>
2018-10-19 20:37:48 +0700

    Remove ViewState For My Cart Menu.

--- HOL__PSD/View/Main.Master.cs
+++ HOL__PSD/View/Main.Master.cs
@@ -6,8 +6,7 @@ namespace HOL__PSD.View
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (ViewState["auth_user"] == null)
-                Cart.Visible = false;
+
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
