666eadd06ed7ced9d9681c02c1e1b17a8458798a
Ivan Elv <purenaturalism@gmail.com>
2018-10-21 12:11:51 +0700

    Add DbManager.cs

---
+++ HOL__PSD/Util/DbManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace HOL__PSD.Util
+{
+    public class DbManager
+    {
+        static SqlConnection connection = null;
+
+        private DbManager() { }
+
+        public static SqlConnection GetInstance()
+        {
+            if (DbManager.connection == null)
+            {
+                string attachDbFilename = @"F:\! Work\HOL PSD\HOL__PSD\HOL__PSD\App_Data\WatchShop.mdf";
+                DbManager.connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + attachDbFilename + ";Integrated Security=True");
+            }
+
+            return DbManager.connection;
+        }
+
+        public static DataTable Get(string query)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection connection = GetInstance())
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataReader rd = cmd.ExecuteReader();
+                dt.Load(rd);
+                connection.Close();
+            }
+
+            return dt;
+        }
+    }
+}
--- HOL__PSD/View/Index.aspx.cs
+++ HOL__PSD/View/Index.aspx.cs
@@ -6,7 +6,7 @@ namespace HOL__PSD.View
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
