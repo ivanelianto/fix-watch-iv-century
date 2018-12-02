d16cdf5e1c3d33a695ca31538e8dc03c9edda518
Ivan Elv <purenaturalism@gmail.com>
2018-12-02 16:04:09 +0700

    Add New Factory Folder

---
+++ HOL__PSD/Factory/ProductFactory.cs
@@ -0,0 +1,36 @@
+﻿using HOL__PSD.Model;
+
+namespace HOL__PSD.Factory
+{
+    public static class ProductFactory
+    {
+        public static Product Create()
+        {
+            return new Product();
+        }
+
+        public static Product Create(string name, decimal price)
+        {
+            Product product = new Product()
+            {
+                name = name,
+                price = price
+            };
+
+            return product;
+        }
+
+        public static Product Create(int id, string name, decimal price, int stock)
+        {
+            Product product = new Product()
+            {
+                id = id,
+                name = name,
+                price = price,
+                stock = stock
+            };
+
+            return product;
+        }
+    }
+}
